034a1a2fd8f80762d2a103be3952b528a6f8883a
Fazley Rabbi <fazleyrabbi555@gmail.com>
2018-09-24 02:24:46 -0400

    edited code

--- GreetingRunnable.java
+++ GreetingRunnable.java
@@ -1,34 +1,32 @@
 
 import java.util.Date;
-/**
-A runnable that repeatedly prints a greeting.
-*/
+
 public class GreetingRunnable implements Runnable
 {
-private static final int REPETITIONS = 4;
-private static final int DELAY = 4;
-private String greeting;
-/**
+    private static final int REPETITIONS = 4;
+	private static final int DELAY = 4;
+	private String greeting;
+/*
 Constructs the runnable object.
 @param aGreeting the greeting to display
 */
-public GreetingRunnable(String aGreeting)
-{
-greeting = aGreeting;
+	public GreetingRunnable(String aGreeting)
+	{
+	greeting = aGreeting;
 }
-public void run()
+	public void run()
 {
-try
+	try
 {
-for (int i = 1; i <= REPETITIONS; i++)
+	for (int i = 1; i <= REPETITIONS; i++)
 {
-Date now = new Date();
-System.out.println(now + " " + greeting);
-Thread.sleep(DELAY);
+	Date now = new Date();
+	System.out.println(now + " " + greeting);
+	Thread.sleep(DELAY);
+	}
 }
-}
-catch (InterruptedException exception)
+	catch (InterruptedException exception)
 {
-}
-}
+		}
+	}
 }
