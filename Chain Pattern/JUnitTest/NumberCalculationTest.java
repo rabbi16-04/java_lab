93cdf266af842a8dcfb61c74ee70121c29b66537
Fazley Rabbi <fazleyrabbi555@gmail.com>
2018-11-11 16:26:00 +0600

    Add files via upload

---
+++ Chain Pattern/JUnitTest/NumberCalculationTest.java
@@ -0,0 +1,21 @@
+
+package chainPattern;
+
+public class NumberCalculationTest {
+
+	public static void main(String[] args) {
+		Chain chainCalc1 = new AddNumbers();
+		Chain chainCalc2 = new SubNumbers();
+		Chain chainCalc3 = new MultiplyNumbers();
+		Chain chainCalc4 = new DivNumbers();
+		
+		chainCalc1.setNextChain(chainCalc2);
+		chainCalc2.setNextChain(chainCalc3);
+		chainCalc3.setNextChain(chainCalc4);
+		
+		Numbers request = new Numbers(4,2,"pow");
+		chainCalc1.calculate(request);
+
+	}
+
+}
